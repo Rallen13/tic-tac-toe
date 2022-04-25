924b862fbae75e1f9630d2fb65f0f9d41cbd82a3
Rachel Allen <rallenco13@gmail.com>
2022-04-25 10:33:35 -0600

    added images to assets and completed HTML skeleton

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 class Game {
   constructor() {
-    
+  this.player1 = new Player(1, "🤖");
+  this.player2 = new Player(2, "🧑🏼‍🚀");
+  this.complete = false;
+  this.turn = this.player1.token;
   }
 }
